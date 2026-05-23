8c9206c2ad03dd1f9fc4564467a983b85b5ead50
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-05-23 20:25:07 +0200

    feat: practice container

---
+++ SomeAlgorithms/Practice/PracticeLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace PracticeLibrary;
+
+public class Class1
+{
+    public static int MaxArea(int[] heights)
+    {
+        var left = 0;
+        var right = heights.Length - 1;
+
+        var maxArea = 0;
+
+        while(left < right)
+        {
+            var width = right - left;
+            var containerHeight = Math.Min(heights[left], heights[right]);
+
+            var area = containerHeight * width;
+
+            if (area > maxArea)
+                maxArea = area;
+
+            if (heights[left] < heights[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
