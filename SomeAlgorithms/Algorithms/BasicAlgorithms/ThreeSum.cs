56692e7736f51186434ff8396e80a2c36f74614d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-05-26 20:44:24 +0200

    feat: practice 3sum

--- SomeAlgorithms/Algorithms/BasicAlgorithms/ThreeSum.cs
+++ SomeAlgorithms/Algorithms/BasicAlgorithms/ThreeSum.cs
@@ -56,7 +56,7 @@ public static class ThreeSum
                     while (left < right && nums[left] == nums[left - 1])
                         left++;
                     
-                    while (left < right && nums[right] == nums[right - 1])
+                    while (left < right && nums[right] == nums[right + 1])
                         right--;
                 }
                 else if (sum < 0)
---
+++ SomeAlgorithms/Practice/PracticeLibrary/Class3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeLibrary;
+
+internal class Class3
+{
+    public static List<List<int>> Find(int[] numbers)
+    {
+        var results = new List<List<int>>();
+
+        for (int i = 0; i < numbers.Length - 2; i++)
+        {
+            var left = i + 1;
+            var right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                var sum = numbers[i] + numbers[left] + numbers[right];
+
+                if (sum == 0)
+                {
+                    results.Add(new List<int>([numbers[i], numbers[left], numbers[right]]));
+                    left++;
+                    right--;
+
+                    while (left < right && numbers[left] == numbers[left - 1])
+                    {
+                        left++;
+                    }
+
+                    while (left < right && numbers[right] == numbers[right + 1])
+                    {
+                        right--;
+                    }
+                }
+                else if (sum < 0)
+                    left++;
+                else
+                    right--;
+
+            }
+        }
+
+        return results;
+    }
+}
